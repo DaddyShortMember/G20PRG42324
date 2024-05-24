583e9acc29f5db4e903d5fc8c06662bdefa639a9
alejandropelegrin <79971607+alejandropelegrin@users.noreply.github.com>
2024-05-24 19:19:34 +0200

    Update ticket.h

--- Fase2/Servidor/ticket.h
+++ Fase2/Servidor/ticket.h
@@ -14,10 +14,8 @@ typedef struct{
 	char date[30];
 }Ticket; //X Bytes, malloc necesario para arrays
 
-//Esta la dejamos para la siguiente, al no ser importante para la primera entrega (el programa es servidor local; esta ya es para el programa tal cual)
-
-Ticket getStop(sqlite3 *db, int id);
-Ticket creaStop(char* name);
+Ticket getTicket(sqlite3 *db, int id);
+Ticket creaTicket(int usid, int tripid, const char* date);
 void anyadirTicket(sqlite3 *db, Ticket ticket);
 
 #endif
